693f5d5c830e7aec3d5711ccdd6e8f04065c5905
Noel Delgado <auhcsei@gmail.com>
2020-05-18 17:15:46 -0600

    :wrench: add rollup.config

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import buble from '@rollup/plugin-buble';
+import replace from '@rollup/plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'index.js',
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+      name: 'mixCssColor'
+    },
+    plugins: [resolve(), commonjs(), buble(), replace({ __VERSION__: `v${pkg.version}` }), terser()]
+  },
+  {
+    input: 'index.js',
+    external: [
+      'parse-css-color',
+      'pure-color/convert/hsl2rgb',
+      'pure-color/convert/rgb2hex',
+      'pure-color/convert/rgb2hsl'
+    ],
+    plugins: [resolve(), commonjs(), replace({ __VERSION__: `v${pkg.version}` })],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ]
+  }
+];
